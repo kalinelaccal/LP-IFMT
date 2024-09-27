5e6f9763a5e83395e7fbf6bd0f30af4bc268c585
kalinelaccal <kalinelaccal@hotmail.com>
2024-09-27 13:21:38 +0000

    Aula 27/09 - comandos basicos

---
+++ matematica.py
@@ -0,0 +1,9 @@
+def area(largura, comprimento):
+    return largura * comprimento
+
+print(area(3,4))
+
+def volume(largura, comprimento, altura):
+    return area(largura, comprimento) * altura
+
+print(volume(3,4,2))
---
+++ ola.py
@@ -0,0 +1,14 @@
+
+#nome = Kaline
+#idade = 28
+#altura = 1.52
+#COMANDO PRA RODAR python3 nomedoarquivo.py
+#COMANDO PRA RODAR SEM QUITAR python3 -i nomedoarquivo.py
+
+
+def ola(nome):#variável local
+    print(f'Olá {nome}! Texto texto texto')
+#ola('Kaline')
+ola(input('Digite seu nome: '))
+
+
